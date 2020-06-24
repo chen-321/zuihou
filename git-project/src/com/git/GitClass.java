5956a05d87b4c6eefaf445329070d0330d1f15cb
huangbangyi2019 <1094476088@qq.com>
2020-06-24 10:19:33 +0800

    add file

---
+++ git-project/src/com/git/GitClass.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class GitClass {
+
+	private int id;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
